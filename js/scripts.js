2d8c39ba0129b048511adbe01c068ccad7eeefb6
Charles Peden <ccbpeden@warpmail.net>
2017-01-17 12:46:46 -0800

    add html to indicate result, front end js to support

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,20 @@ var leapYear = function(year) {
 
 // jQuery user interface logic here.
 
-
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
   });
 });
